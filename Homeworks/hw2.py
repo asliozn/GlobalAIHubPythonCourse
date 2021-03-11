cc4f67902a6cc90f91c852fd4d3f417f924a4813
Aslı <asliiozen@hotmail.com>
2021-03-11 12:10:32 +0300

    Update hw2.py

--- Homeworks/hw2.py
+++ Homeworks/hw2.py
@@ -1,12 +1,17 @@
-cv1={"Name":"Aslı","Surname":"Özen","Job Title":"Computer Engineer","Mail":"aslozn@hotmail.com","Education":"Kocaeli University "}
-cv2={"Name":"Hazal","Surname":"Saç","Job Title":"Industrial Engineer","Mail":"hazlsac@gmail.com","Education":"Middle East Technical University "}
-cv3={"Name":"Melek","Surname":"Coşkun","Job Title":"Dentist","Mail":"melekcskn@hotmail.com","Education":"Marmara University "}
-cv4={"Name":"Beyza","Surname":"Akbaş","Job Title":"Chemical Engineer","Mail":"byzaakbas@hotmail.com","Education":"Gazi University "}
-cv5={"Name":"Sema","Surname":"Gümüş","Job Title":"Chemical Engineer","Mail":"semagumus2@hotmail.com","Education":"Middle East Technical University "}
-cvlist=[cv1,cv2,cv3,cv4,cv5]
-
-for i in range(5):
- print("--------------- CV",i+1,"-----------------")
- for k,v in cvlist[i].items():
-  print( k,":", v)
-
+cv1 = {"Name": "Aslı", "Surname": "Özen", "Job Title": "Computer Engineer", "Mail": "aslozn@hotmail.com",
+       "Education": "Kocaeli University "}
+cv2 = {"Name": "Hazal", "Surname": "Saç", "Job Title": "Industrial Engineer", "Mail": "hazlsac@gmail.com",
+       "Education": "Middle East Technical University "}
+cv3 = {"Name": "Melek", "Surname": "Coşkun", "Job Title": "Dentist", "Mail": "melekcskn@hotmail.com",
+       "Education": "Marmara University "}
+cv4 = {"Name": "Beyza", "Surname": "Akbaş", "Job Title": "Chemical Engineer", "Mail": "byzaakbas@hotmail.com",
+       "Education": "Gazi University "}
+cv5 = {"Name": "Sema", "Surname": "Gümüş", "Job Title": "Chemical Engineer", "Mail": "semagumus2@hotmail.com",
+       "Education": "Middle East Technical University "}
+cvlist = [cv1, cv2, cv3, cv4, cv5]
+i = 1
+for cv in cvlist:
+    print("--------------- CV", i, "-----------------")
+    i = i + 1
+    for k, v in cv.items():
+        print(k, ":", v)
